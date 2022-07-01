98ca3f65fbb97b75dc0d47cbdb4ee184565faafd
EMEJUO KELECHI <emejuokelechi@gmail.com>
2022-07-01 15:26:40 -0700

    print lower and upper case alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Retrun: Always 0 (Success)
+*/
+int main(void)
+{
+	char a, A;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	for (A = 'A'; A <= 'Z'; A++)
+	{
+		putchar(A);
+	}
+	putchar('\n');
+	return (0);
+}
