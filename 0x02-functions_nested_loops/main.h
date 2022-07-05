b7a39c3c62491ee74f8657f0f71eb6693fb8b947
EMEJUO KELECHI <emejuokelechi@gmail.com>
2022-07-05 11:47:28 -0700

    fix

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,3 @@
-#ifndef main
-#define main
-
-#include <stdio.h>
-
-
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -18,5 +12,3 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-
-#endif /* main */
