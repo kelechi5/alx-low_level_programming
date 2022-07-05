d2ed305268967b6c81da411e3d1505ce9e7b3463
EMEJUO KELECHI <emejuokelechi@gmail.com>
2022-07-05 10:57:02 -0700

    task 0

--- 0x02-functions_nested_loops/0-puthchar.c
+++ 0x02-functions_nested_loops/0-puthchar.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
-  * main - check the code for ALX School students.
-  *
-  * Return: Always 0.
-  */
+* main - check the code for ALX School students.
+*
+* Return: Always 0.
+*/
+
 int main (void)
 {
 	_putchar('_');
