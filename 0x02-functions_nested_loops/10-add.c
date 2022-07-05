ba7c6f47918cddf01a9bef91c5b55c1b39412730
EMEJUO KELECHI <emejuokelechi@gmail.com>
2022-07-05 13:38:42 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - add two int
+ *
+ * @i: first int
+ * @j: secont int
+ * Return: result
+ */
+
+int add(int i, int j)
+{
+	int res;
+	res= i + j;
+	return (res);
+}
