1b67b4a7791d455b7b26d0fa853a3bcfab94f7fa
EMEJUO KELECHI <emejuokelechi@gmail.com>
2022-07-14 11:57:25 -0700

    task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints an integer.
+ * @n: given number.
+ */
+
+void print_number(int n)
+{
+	int add = 0;
+
+	if (n == -2147483648)
+	{
+		n = n + 1;
+		add = 1;
+	}
+
+	if (n < 0)
+	{
+		_putchar ('-');
+		n = n;
+	}
+
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	if (add == 1)
+	{
+		_putchar ('8');
+	}
+	else _putchar (n % 10 + '0');
+}
