a91c8f9e5f1d26b68967486fb8afd69af8c00c08
EMEJUO KELECHI <emejuokelechi@gmail.com>
2022-07-14 12:38:17 -0700

    hot fix

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,29 +3,39 @@
 /**
  * print_number - function that prints an integer.
  * @n: given number.
+ * Return: void
  */
+int base10(int n)
+{
+	int base = 10;
+
+	while (n > 0)
+	{
+		base *= 10;
+		n--;
+	}
+	return (base);
+}
 
 void print_number(int n)
 {
-	int add = 0;
+	int power;
 
-	if (n == -2147483648)
-	{
-		n = n + 1;
-		add = 1;
-	}
+	power = base10(8);
 
-	else if (n < 0)
-	{
-		_putchar ('-');
-		n = -n;
-	}
+	if (n == 0)
+		_putchar('0');
 
-	else if (n / 10)
-		print_number(n / 10);
-
-	else if (add == 1)
-		_putchar ('8');
 	else
-		_putchar (n % 10 + '0');
+	{
+		while (n / power == 0)
+			power /= 10;
+
+		while (power >=1)
+		{
+			_putchar(n / power) + '0');
+			n %= power;
+			power /= 10;
+		}
+	}
 }
