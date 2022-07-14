b82ce60b70da0059c25ad3d27f74317f15acf703
EMEJUO KELECHI <emejuokelechi@gmail.com>
2022-07-14 08:05:38 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * *cap_string - function that capitalizes all words of a string.
+ * @s: given string
+ * Return: string.
+ */
+
+char *cap_string(char *s)
+{
+	int ia = 0;
+	int ib;
+
+	char str[] = "  \t\n,;.!,?\"(){}\0";
+
+	if (s[ia] >= 'a' && s[ia] <= 'z')
+		s[ia] = s[ia] - 32;
+
+	while (s[ia] != '\0')
+	{
+		if (s[ia] >= 'a' && s[ia] <= 'z')
+		{
+			for (ib = 0; str[ib] != '\0'; ib++)
+			{
+				if (s[ia - 1] == str[ib])
+					s[ia] = s[ia] - 32;
+			}
+		}
+		ia++;
+	}
+	return (s);
+}
