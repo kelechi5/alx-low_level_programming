dbfbf40c9e6621251fc1f3754f35685613544a90
EMEJUO KELECHI <emejuokelechi@gmail.com>
2022-07-21 04:07:44 -0700

    task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - rev
+ * @s: pointer to string parameters
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
